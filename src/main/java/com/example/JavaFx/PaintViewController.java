a7d321e65b2369a62b516700272ee7b0d4d4eba1
wassimtajeddin <wassim.tajeddin@gmail.com>
2023-08-19 12:35:26 +0200

    Extracting methods

--- src/main/java/com/example/JavaFx/PaintViewController.java
+++ src/main/java/com/example/JavaFx/PaintViewController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
@@ -51,69 +52,88 @@ public class PaintViewController {
             size = Double.parseDouble(sizeField.getText());
         return size;
     }
+    Rectangle rectangle = new Rectangle();
+    Circle circle = new Circle();
 
     public void onCanvasClicked() {
 
 
-        Rectangle rectangle = new Rectangle();
-        Circle circle = new Circle();
-
         graphicsContext.setFill(colorPicker.getValue());
-
         graphicsContext.setLineWidth(1);
 
         canvas.setOnMousePressed(e -> {
-
             if (rectangleButton.isFocused()) {
-                rectangle.setWidth(Math.abs((e.getX() - rectangle.getX())));
-                rectangle.setHeight(Math.abs((e.getY() - rectangle.getY())));
-
-
-                graphicsContext.fillRect(e.getX(), e.getY(), size(), size());
-
-                undoHistory.push(new Rectangle(e.getX(), e.getY(), size(), size()));
-
+                drawRectangle(e);
             } else if (circleButton.isFocused()) {
-                circle.setRadius((Math.abs(e.getX() - circle.getCenterX()) + Math.abs(e.getY() - circle.getCenterY())) / 2);
-
-                if (circle.getCenterX() > e.getX()) {
-                    circle.setCenterX(e.getX());
-                }
-                if (circle.getCenterY() > e.getY()) {
-                    circle.setCenterY(e.getY());
-                }
-
-                graphicsContext.fillOval(e.getX(), e.getY(), size(), size());
-
-                undoHistory.push(new Circle(e.getX(), e.getY(), size()));
+                drawCircle(e);
             }
-            redoHistory.clear();
-            Shape lastUndo = undoHistory.lastElement();
-            lastUndo.setFill(graphicsContext.getFill());
+            clearRedoHistory();
+            setLastUndoFill();
+        });
+    }
 
-        });}
+    private void drawRectangle(MouseEvent event) {
+        double size = size();
+        rectangle.setWidth(Math.abs((event.getX() - rectangle.getX())));
+        rectangle.setHeight(Math.abs((event.getY() - rectangle.getY())));
+
+        graphicsContext.fillRect(event.getX(), event.getY(), size, size);
+
+        undoHistory.push(new Rectangle(event.getX(), event.getY(), size, size));
+    }
+
+    private void drawCircle(MouseEvent event) {
+        double size = size();
+        double centerX = Math.min(event.getX(), circle.getCenterX());
+        double centerY = Math.min(event.getY(), circle.getCenterY());
+        circle.setRadius((Math.abs(event.getX() - circle.getCenterX()) + Math.abs(event.getY() - circle.getCenterY())) / 2);
+        circle.setCenterX(centerX);
+        circle.setCenterY(centerY);
+
+        graphicsContext.fillOval(event.getX(), event.getY(), size, size);
+
+        undoHistory.push(new Circle(event.getX(), event.getY(), size));
+    }
+
+    private void clearRedoHistory() {
+        redoHistory.clear();
+    }
+
+    private void setLastUndoFill() {
+        Shape lastUndo = undoHistory.lastElement();
+        lastUndo.setFill(graphicsContext.getFill());
+    }
+
 
 
     public void onSaveAction() {
-        saveButton.setOnAction((e) -> {
-            FileChooser saveFile = new FileChooser();
-            saveFile.setTitle("Save File");
-            saveFile.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG files", "*.png"));
-            File file = saveFile.showSaveDialog(stage);
-            System.out.println("Success " + file);
-            if (file != null) {
-                try {
-                    WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
-                    canvas.snapshot(null, writableImage);
-                    RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
-                    ImageIO.write(renderedImage, "png", file);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                    System.out.println("Error!");
-                }
-            }
-        });
+        saveButton.setOnAction(e -> saveToFile());
     }
+
+    private void saveToFile() {
+        FileChooser saveFile = new FileChooser();
+        saveFile.setTitle("Save File");
+        saveFile.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG files", "*.png"));
+        File file = saveFile.showSaveDialog(stage);
+
+        if (file != null) {
+            saveCanvasToFile(file);
+        }
+    }
+
+    private void saveCanvasToFile(File file) {
+        try {
+            WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+            canvas.snapshot(null, writableImage);
+            RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+            ImageIO.write(renderedImage, "png", file);
+            System.out.println("Success: " + file);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Error!");
+        }
+    }
+
 
     public void onUndoAction() {
         undoButton.setOnAction(e -> undoLastAction());
@@ -134,48 +154,35 @@ public class PaintViewController {
             lastRedo.setStroke(removedShape.getStroke());
             lastRedo.setStrokeWidth(removedShape.getStrokeWidth());
             undoHistory.pop();
-
-            for(int i=0; i < undoHistory.size(); i++) {
-                Shape shape = undoHistory.elementAt(i);
-
-                if(shape.getClass() == Rectangle.class) {
-                    undoOneRectangleShape();
-                }
-                else if(shape.getClass() == Circle.class) {
-                    undoOneCircleShape();
-                }
-
-            }
-
+            undoOneShapeForEachClick();
         } else {
             System.out.println("There is no action to undo");
         }
     }
-    private void undoOneRectangleShape(){
+    private void undoOneShapeForEachClick(){
         for(int i=0; i < undoHistory.size(); i++) {
             Shape shape = undoHistory.elementAt(i);
-            Rectangle temp = (Rectangle) shape;
-            graphicsContext.setFill(temp.getFill());
-            graphicsContext.fillRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
+
+            if(shape.getClass() == Rectangle.class) {
+                Rectangle temp = (Rectangle) shape;
+                graphicsContext.setFill(temp.getFill());
+                graphicsContext.fillRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
+            }
+            else if(shape.getClass() == Circle.class) {
+                Circle temp = (Circle) shape;
+                graphicsContext.setFill(temp.getFill());
+                graphicsContext.fillOval(temp.getCenterX(), temp.getCenterY(), temp.getRadius(), temp.getRadius());
+            }
+
         }
 
     }
-    private void undoOneCircleShape() {
-        for (int i = 0; i < undoHistory.size(); i++) {
-            Shape shape = undoHistory.elementAt(i);
-            Circle temp = (Circle) shape;
-            graphicsContext.setFill(temp.getFill());
-            graphicsContext.fillOval(temp.getCenterX(), temp.getCenterY(), temp.getRadius(), temp.getRadius());
 
-        }
-    }
     private void undoRectangleAction(Rectangle removedRectangle) {
         removedRectangle.setFill(graphicsContext.getFill());
         removedRectangle.setStroke(graphicsContext.getStroke());
         removedRectangle.setStrokeWidth(graphicsContext.getLineWidth());
         redoHistory.push(new Rectangle(removedRectangle.getX(), removedRectangle.getY(), removedRectangle.getWidth(), removedRectangle.getHeight()));
-
-
     }
 
     private void undoCircleAction(Circle removedCircle) {
